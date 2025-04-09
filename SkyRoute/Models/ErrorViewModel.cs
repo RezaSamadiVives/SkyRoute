56ebbfa220a414894db28039bcbdab2a01d15d71
Reza Samadi GFN <reza_samadi_rns@hotmail.com>
2025-04-09 15:04:23 +0200

    Add project files.

---
+++ SkyRoute/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SkyRoute.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
