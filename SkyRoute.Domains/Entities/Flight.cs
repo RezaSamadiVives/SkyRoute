bee13cc2c3b25ce3a17aeaa837faec9e0c471d7a
Reza Samadi GFN <reza_samadi_rns@hotmail.com>
2025-04-20 13:15:55 +0200

    city and flight entities added

---
+++ SkyRoute.Domains/Entities/Flight.cs
@@ -0,0 +1,22 @@
+﻿namespace SkyRoute.Domains.Entities
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+
+        public int FromCityId { get; set; }
+        public City FromCity { get; set; } = null!;
+
+        public int ToCityId { get; set; }
+        public City ToCity { get; set; } = null!;
+
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+        public string FlightNumber { get; set; } = null!;
+        public bool HasStopover { get; set; }
+
+        public string? StopoverCities { get; set; }
+    }
+
+}
