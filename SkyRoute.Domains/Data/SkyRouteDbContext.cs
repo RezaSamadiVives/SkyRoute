3409b2d813765c771b4e1b6fc1a089760e75677a
Reza Samadi GFN <reza_samadi_rns@hotmail.com>
2025-04-20 16:01:13 +0200

    dbcontext added

---
+++ SkyRoute.Domains/Data/SkyRouteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyRoute.Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyRoute.Domains.Data
+{
+    public class SkyRouteDbContext : DbContext
+    {
+        public SkyRouteDbContext(DbContextOptions<SkyRouteDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
