849dfb9f14a5a39dd3a7c62cfb08d3d4f95d23ab
Reza Samadi GFN <reza_samadi_rns@hotmail.com>
2025-04-28 23:04:09 +0200

    layout home finished for now

--- SkyRoute/Controllers/HomeController.cs
+++ SkyRoute/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SkyRoute.Models;
+using SkyRoute.ViewModels;
 using System.Diagnostics;
 
 namespace SkyRoute.Controllers
@@ -15,10 +17,29 @@ namespace SkyRoute.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model = new HomeVM
+            {
+                Cities =
+                [
+                    new SelectListItem { Value = "NYC", Text = "New York" },
+                    new SelectListItem { Value = "LON", Text = "Londen" },
+                    new SelectListItem { Value = "TYO", Text = "Tokio" },
+                    new SelectListItem { Value = "SYD", Text = "Sydney" },
+                    new SelectListItem { Value = "DXB", Text = "Dubai" },
+                    new SelectListItem { Value = "CPT", Text = "Kaapstad" },
+                    new SelectListItem { Value = "SIN", Text = "Singapore" }
+                ]
+            };
+            
+            return View(model);
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
---
+++ SkyRoute/ViewModels/HomeVM.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SkyRoute.ViewModels
+{
+    public class HomeVM
+    {
+        public required List<SelectListItem> Cities { get; set; }
+    }
+}
